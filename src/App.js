1d140f6f76d6e4a9ee984779dcf425a79da23465
nicoacevedo1 <niiko0495@gmail.com>
2022-06-22 02:29:12 -0300

    Se agrega contador

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Body from './components/Body';
 import Container from './components/Container';
+import Itemcount from './components/ItemCount';
 
 function App() {
   return (
     <div>
       <Navbar />
       <Body />
+      <Itemcount />
     </div>
 
   );
